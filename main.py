75af6b97521131d40c6d1b2dfed9adf1bce6a8f8
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-24 19:26:53 +0800

    Update UI

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECK_MARK = "✓"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -25,16 +26,16 @@ canvas.create_image(100, 112, image=tomate_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, 'bold'))
 canvas.grid(column=1, row=1)
 
-label_timer = Label(text='Timer', font=(FONT_NAME, 24, 'normal'), bg=YELLOW, fg=GREEN)
+label_timer = Label(text='Timer', font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
 label_timer.grid(column=1, row=0)
 
-button_start = Button(text='Start', font=(FONT_NAME, 10, 'normal'))
+button_start = Button(text='Start', highlightthickness=0)
 button_start.grid(column=0, row=2)
 
-button_reset = Button(text='Reset', font=(FONT_NAME, 10, 'normal'))
+button_reset = Button(text='Reset', highlightthickness=0)
 button_reset.grid(column=2, row=2)
 
-pomodoro_counter = Label(text='✓', font=(FONT_NAME, 12, 'normal'), bg=YELLOW, fg=GREEN)
+pomodoro_counter = Label(text=CHECK_MARK, bg=YELLOW, fg=GREEN)
 pomodoro_counter.config(pady=20)
 pomodoro_counter.grid(column=1, row=2)
 
