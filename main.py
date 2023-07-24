d29d2f442381610c1a0540a29e52f4a66d5aacf8
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-24 19:23:29 +0800

    Finish UI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,13 +16,26 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
-window = tkinter.Tk()
+window = Tk()
 window.title("Pomodoro App")
 window.config(padx=100, pady=50, bg=YELLOW)
-canvas = tkinter.Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
-tomate_img = tkinter.PhotoImage(file="tomato.png")
+canvas = Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
+tomate_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomate_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, 'bold'))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+label_timer = Label(text='Timer', font=(FONT_NAME, 24, 'normal'), bg=YELLOW, fg=GREEN)
+label_timer.grid(column=1, row=0)
+
+button_start = Button(text='Start', font=(FONT_NAME, 10, 'normal'))
+button_start.grid(column=0, row=2)
+
+button_reset = Button(text='Reset', font=(FONT_NAME, 10, 'normal'))
+button_reset.grid(column=2, row=2)
+
+pomodoro_counter = Label(text='✓', font=(FONT_NAME, 12, 'normal'), bg=YELLOW, fg=GREEN)
+pomodoro_counter.config(pady=20)
+pomodoro_counter.grid(column=1, row=2)
 
 window.mainloop()
